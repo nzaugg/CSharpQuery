fe5314806067ee2a383008d1d615b7c7599b9c4e
Darren Cauthon <darren@cauthon.com>
2010-10-18 21:30:58 -0500

    Minor refactor, use getters.

--- CSharpQuery.Index/TextIndexFileInformation.cs
+++ CSharpQuery.Index/TextIndexFileInformation.cs
@@ -2,12 +2,39 @@
 {
     public class TextIndexFileInformation
     {
-        public string BeginFile = (char)01 + "CSharpQuery_Index" + (char)01;
-        public string BeginRecord = (char)02 + "";
-        public string BeginField = (char)03 + "";
-        public string FieldInfoDelimeter = (char)04 + "";
-        public string EndField = (char)05 + "";
-        public string EndRecord = (char)06 + "\r\n";
-        public string EndFile = (char)07 + "";
+        public virtual string BeginFile
+        {
+            get { return (char) 01 + "CSharpQuery_Index" + (char) 01; }
+        }
+
+        public virtual string BeginRecord
+        {
+            get { return (char) 02 + ""; }
+        }
+
+        public virtual string BeginField
+        {
+            get { return (char) 03 + ""; }
+        }
+
+        public virtual string FieldInfoDelimeter
+        {
+            get { return (char) 04 + ""; }
+        }
+
+        public virtual string EndField
+        {
+            get { return (char) 05 + ""; }
+        }
+
+        public virtual string EndRecord
+        {
+            get { return (char) 06 + "\r\n"; }
+        }
+
+        public virtual string EndFile
+        {
+            get { return (char) 07 + ""; }
+        }
     }
 }
