af9f9a174e8cdd592f9455a435086ff06d337079
Nate Zaugg <nzaugg@gmail.com>
2015-09-24 18:04:03 -0600

    Creating NuGet package

--- CSharpQuery.WordBreaker/Properties/AssemblyInfo.cs
+++ CSharpQuery.WordBreaker/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CSharpQuery.WordBreaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Software Interactive Corp")]
+[assembly: AssemblyCompany("Mindfire Technology")]
 [assembly: AssemblyProduct("CSharpQuery.WordBreaker")]
-[assembly: AssemblyCopyright("Copyright © Software Interactive Corp 2009")]
+[assembly: AssemblyCopyright("Copyright ©  Mindfire Technology")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
